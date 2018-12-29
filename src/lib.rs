1c10fed2db4d7014fb21ce171898b52960aeaced
tga <tga@wengwengweng.me>
2018-12-28 20:14:37 -0500

    misc

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,7 @@ pub mod app;
 pub mod gfx;
 pub mod audio;
 pub mod math;
-
-#[cfg(feature = "res")]
 pub mod res;
-#[cfg(feature = "col")]
 pub mod col;
-#[cfg(feature = "lua")]
 pub mod lua;
 
--- src/res.rs
+++ src/res.rs
@@ -1,1 +1,46 @@
 // wengwengweng
+
+use std::fs;
+use crate::*;
+
+ctx!(RES: ResCtx);
+
+struct ResCtx {
+	sprites: Vec<SpriteData>,
+}
+
+pub fn init() {
+
+	init_ctx(ResCtx {
+		sprites: vec![],
+	});
+
+}
+
+struct SpriteData {
+	tex: gfx::Texture,
+	frames: Vec<math::Vector4>,
+}
+
+pub fn load_sprites(dir: &str, names: Vec<&str>) {
+
+	for name in names {
+		// ...
+	}
+
+}
+
+pub fn load_all_sprites(dir: &str) {
+
+	let files = fs::read_dir(dir).unwrap();
+
+	for file in files {
+		// ...
+	}
+
+}
+
+pub fn get_sprite(name: &str) {
+	// ...
+}
+
