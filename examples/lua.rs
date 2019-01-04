580cedbd56dc032fdc78d9a66fdf8911ea905a7d
tga <tga@wengwengweng.me>
2019-01-04 12:18:44 -0500

    add cli

---
+++ examples/lua.rs
@@ -0,0 +1,11 @@
+// wengwengweng
+
+use dirty::*;
+use dirty::math::*;
+
+fn main() {
+	lua::run(include_str!("test.lua"));
+}
+
+
+
---
+++ examples/test.lua
@@ -0,0 +1,10 @@
+-- wengwengweng
+
+app_init()
+window_init("yo", 640, 480)
+gfx_init()
+
+app_run(function()
+	gfx_clear()
+end)
+
