01a607fd004355865695451d809ed4d55bd0e56a
tga <tga@space55.xyz>
2019-01-09 22:28:16 -0500

    audio effectsj

--- src/modules/audio.rs
+++ src/modules/audio.rs
@@ -3,10 +3,15 @@
 //! Sound loading & playback
 
 use std::io::Cursor;
+use std::time::Duration;
 
 use rodio::Source;
 use rodio::Decoder;
+use rodio::Sink;
+use rodio::Sample;
 use rodio::source::Buffered;
+use rodio::source::Speed;
+use rodio::source::Delay;
 
 use crate::*;
 
@@ -36,26 +41,34 @@ pub fn enabled() -> bool {
 }
 
 /// play a given sound once till end
-pub fn play(sound: &Sound) {
+pub fn effect<S>(sound: &Sound<S>)
+where
+	S: Source<Item = i16> + Send + 'static, {
 	rodio::play_raw(&ctx_get().device, sound.buffer.clone().convert_samples());
 }
 
 /// a sound is meant to play just once
-pub struct Sound {
-	/// buffer
-	buffer: Buffered<Decoder<Cursor<Vec<u8>>>>,
+pub struct Sound<I>
+where
+	I: Source,
+	I::Item: Sample, {
+
+	buffer: Buffered<I>,
+
 }
 
-impl Sound {
+pub(crate) type SoundData = Sound<Decoder<Cursor<Vec<u8>>>>;
+
+impl SoundData {
 
 	/// create a sound from bytes
 	pub fn from_bytes(data: &[u8]) -> Self {
 
 		let cursor = Cursor::new(data.to_owned());
-		let source = Decoder::new(cursor).expect("failed to decode sound");
+		let decoder = Decoder::new(cursor).expect("failed to decode sound");
 
 		return Self {
-			buffer: source.buffered(),
+			buffer: decoder.buffered(),
 		};
 
 	}
@@ -63,6 +76,47 @@ impl Sound {
 	/// create a sound from file
 	pub fn from_file(fname: &str) -> Self {
 		return Self::from_bytes(&fs::read_bytes(fname));
+	}
+
+}
+
+impl<I> Sound<I>
+where
+	I: Source,
+	I::Item: Sample, {
+
+	/// get a sound with changed speed
+	pub fn speed(&self, f: f32) -> Sound<Speed<Buffered<I>>> {
+		return Sound {
+			buffer: self.buffer.clone().speed(f).buffered(),
+		}
+	}
+
+	/// get a sound with delay effect
+	pub fn delay(&self, f: u64) -> Sound<Delay<Buffered<I>>> {
+		return Sound {
+			buffer: self.buffer.clone().delay(Duration::from_millis(f)).buffered(),
+		}
+	}
+
+}
+
+/// a track has more control
+pub struct Track {
+	sink: Sink,
+}
+
+/// play a sound and return a track
+pub fn play<S>(sound: &Sound<S>) -> Track
+where
+	S: Source<Item = i16> + Send + 'static, {
+
+	let sink = Sink::new(&ctx_get().device);
+
+	sink.append(sound.buffer.clone());
+
+	return Track {
+		sink: sink,
 	}
 
 }
