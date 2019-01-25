fee960ed8b4182e75b92e13621dfd01a55c27e03
tga <tga@wengwengweng.me>
2019-01-25 00:52:19 -0500

    add window global scale

--- examples/specs/main.rs
+++ examples/specs/main.rs
@@ -31,10 +31,8 @@ fn main() {
 	window::init("yo", 640, 480);
 	res::init();
 
-	window::scale(window::Scale::X2);
-
+	window::scale(window::Scale::X4);
 	app::set_debug(true);
-
 	res::load_sprites("examples/assets/", &vec!["core", "petal"]);
 
 	let mut world = World::new();
@@ -75,9 +73,23 @@ fn create_flower(world: &mut World, p: Player, pos: Vec2) -> Entity {
 
 	return world
 		.create_entity()
-		.with(Trans::new(pos, 0.0, vec2!(2)))
+		.with(Trans::new(pos, 0.0, vec2!(1)))
 		.with(Vel::new(vec2!()))
 		.with(flower)
+		.with(Body::new(&sprite.get_verts()))
+		.with(sprite)
+		.build();
+
+}
+
+fn create_petal(world: &mut World, flower: Entity) -> Entity {
+
+	let sprite = Sprite::new("petal");
+
+	return world
+		.create_entity()
+		.with(Trans::default())
+		.with(Vel::new(vec2!()))
 		.with(Body::new(&sprite.get_verts()))
 		.with(sprite)
 		.build();
