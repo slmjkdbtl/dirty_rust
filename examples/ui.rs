5a49a7f36575b11cfe5a2a1afc088b76d5ae7488
tga <tga@wengwengweng.me>
2019-01-28 16:21:50 -0500

    fix mouse input

--- examples/ui.rs
+++ examples/ui.rs
@@ -13,8 +13,10 @@ fn main() {
 	let (width, height) = window::size();
 
 	let test = ui::Window::new("window", vec2!(24, 24), 240, 320);
+	let game = ui::Window::new("game", vec2!(240, 24), 240, 320);
 
 	ui::add(test);
+	ui::add(game);
 
 	app::run(|| {
 
