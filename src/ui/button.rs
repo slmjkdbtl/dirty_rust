4697b73108c8b3557d93291ce79ebfae72251a5e
tga <tga@space55.xyz>
2019-01-28 21:23:17 -0500

    fix get texture data and gfx::capture

---
+++ src/ui/button.rs
@@ -0,0 +1,35 @@
+// wengwengweng
+
+use super::*;
+use crate::*;
+
+// use utils;
+
+const BUTTON_WIDTH: u32 = 120;
+const BUTTON_HEIGHT: u32 = 64;
+
+/// canvas for drawing custom stuff
+pub struct Button {
+	text: String,
+}
+
+impl Button {
+
+	pub fn new(t: &str) -> Self {
+		return Self {
+			text: String::from(t),
+		};
+	}
+
+}
+
+impl Widget for Button {
+
+	fn draw(&self) {
+// 		gfx::poly(&utils::rounded_rect(BUTTON_WIDTH, BUTTON_HEIGHT, CORNER));
+	}
+
+}
+
+
+
