68c1f625752bc36525444aff944d8fd3f890224e
tga <tga@wengwengweng.me>
2019-05-24 11:49:27 -0400

    use mods instead of globals

--- examples/main.lua
+++ examples/main.lua
@@ -1,8 +1,22 @@
 -- wengwengweng
 
-local win = window.create()
+local window = require("window")
+local audio = require("audio")
+
+local win = window.create({})
+local sound = audio.async_load_file("examples/res/yo.ogg")
 
 win:run(function(ctx)
-	-- ...
+
+	if not sound:done() then
+
+		sound:poll()
+
+		if sound:done() then
+			sound:data():play()
+		end
+
+	end
+
 end)
 
