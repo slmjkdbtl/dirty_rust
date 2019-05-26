7075088b5358d6582cd31b22a09ba9b7f8c1eec8
tga <tga@wengwengweng.me>
2019-05-26 03:06:51 -0400

    add more error handling

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,21 @@ fn main() {
 	let args = env::args().collect::<Vec<String>>();
 
 	if let Some(action) = args.get(1) {
-		if fs::exists(action) {
-			lua::run_file(action, Some(&args[2..args.len()]));
-		} else if action == "export" {
-			// ...
-		} else {
-			eprintln!("not found");
-		}
-	} else {
-		if fs::exists("main.lua") {
-			lua::run(&fs::read_str("main.lua").unwrap(), Some("main.lua"), Some(&args[2..args.len()]));
+
+		if let Ok(code) = fs::read_str(action) {
+			lua::run(&code, Some(action), Some(&args[2..args.len()]));
 		} else {
 			eprintln!("no");
 		}
+
+	} else {
+
+		if let Ok(code) = fs::read_str("main.lua") {
+			lua::run(&code, Some("main.lua"), None);
+		} else {
+			eprintln!("no");
+		}
+
 	}
 
 }
