bd8d692995f8a1a8bb6684242c092284c82fef90
tga <tga@space55.xyz>
2019-05-26 16:53:59 -0400

    add more window stuff

--- examples/window.rs
+++ examples/window.rs
@@ -8,6 +8,9 @@ fn main() {
 	let mut win = window::Window::default();
 
 	win.run(|ctx| {
+		if ctx.key_pressed(Key::F) {
+			ctx.toggle_cursor_locked();
+		}
 		if ctx.key_pressed(Key::Escape) {
 			ctx.close();
 		}
