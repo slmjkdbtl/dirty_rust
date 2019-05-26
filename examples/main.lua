ceffe2f19e261434540e3f3b2c3ce95572286da3
tga <tga@wengwengweng.me>
2019-05-26 02:34:40 -0400

    add term

--- examples/main.lua
+++ examples/main.lua
@@ -2,9 +2,8 @@
 
 local window = require("window")
 local audio = require("audio")
-local ansi = require("ansi")
 
-local win = window.create({})
+local win = window.make({})
 local sound = audio.async_load_file("examples/res/yo.ogg")
 
 win:run(function(ctx)
