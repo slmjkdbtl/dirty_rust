376ac2f18a327511a50d5a1048b5352567345f11
tga <tga@space55.xyz>
2019-08-01 00:21:50 +0800

    add minial window example

---
+++ examples/window.rs
@@ -0,0 +1,36 @@
+// wengwengweng
+
+use dirty::*;
+use dirty::app::*;
+use input::Key;
+
+struct Game;
+
+impl app::State for Game {
+
+	fn init(_: &mut app::Ctx) -> Result<Self> {
+		return Ok(Self);
+	}
+
+	fn run(&mut self, ctx: &mut app::Ctx) -> Result<()> {
+
+		ctx.draw(shapes::text("yo"))?;
+
+		if ctx.key_pressed(Key::Escape) {
+			ctx.quit();
+		}
+
+		return Ok(());
+
+	}
+
+}
+
+fn main() {
+
+	if let Err(err) = app::run::<Game>() {
+		println!("{}", err);
+	}
+
+}
+
