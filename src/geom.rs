5b1a9841ca3a1860b1ff93b82bad97ccc47640ff
tga <tga@wengwengweng.me>
2019-08-16 04:18:07 +0800

    add some geom functions

--- src/geom.rs
+++ src/geom.rs
@@ -2,6 +2,8 @@
 
 //! General Collision Detection
 
+// some code borrowed from [p5.collide2dD](https://github.com/bmoren/p5.collide2D)
+
 use crate::*;
 use crate::math::*;
 
@@ -21,18 +23,102 @@ fn pair<T, F: FnMut(&T, &T)>(list: &[T], mut f: F) {
 
 }
 
-// TODO: use (Vec2, Vec2) for rects
-fn rect_rect(p1: Vec2, p2: Vec2, p3: Vec2, p4: Vec2) -> bool {
+fn rect_rect(r1: (Vec2, Vec2), r2: (Vec2, Vec2)) -> bool {
+
+	let (p1, p2) = r1;
+	let (p3, p4) = r2;
+
 	return p1.x <= p3.x && p2.x >= p4.x && p1.y <= p3.y && p2.y >= p4.y;
 }
 
-// TODO
-fn rect_circle(p1: Vec2, p2: Vec2, center: Vec2, radius: f32) -> bool {
+fn line_rect(line: (Vec2, Vec2), rect: (Vec2, Vec2)) -> bool {
+
+	let (p1, p2) = line;
+	let (p3, p4) = rect;
+
+	if point_rect(p1, rect) || point_rect(p2, rect) {
+		return true;
+	}
+
+	if
+		line_line((p1, p2), (p3, vec2!(p4.x, p3.y)))
+		|| line_line((p1, p2), (vec2!(p4.x, p3.y), p4))
+		|| line_line((p1, p2), (p4, vec2!(p3.x, p4.y)))
+		|| line_line((p1, p2), (vec2!(p3.x, p4.y), p3)) {
+		return true;
+	}
+
 	return false;
+
 }
 
-fn circle_circle(c1: Vec2, r1: f32, c2: Vec2, r2: f32) -> bool {
-	return r1 + r2 < Vec2::dis(c1, c2);
+fn rect_circle(rect: (Vec2, Vec2), circle: (Vec2, f32)) -> bool {
+
+	let (p1, p2) = rect;
+	let (center, radius) = circle;
+	let mut test = center;
+
+	if (center.x < p1.x) {
+		test.x = p1.x;
+	} else if (center.x > p2.x) {
+		test.x = p2.x;
+	}
+
+	if (center.y < p1.y){
+		test.y = p1.y;
+	} else if (center.y > p2.y) {
+		test.y = p2.y;
+	}
+
+	return Vec2::dis(center, test) <= radius;
+
+}
+
+fn point_circle(pt: Vec2, circle: (Vec2, f32)) -> bool {
+	let (center, radius) = circle;
+	return Vec2::dis(pt, center) < radius;
+}
+
+// TODO
+fn line_circle(line: (Vec2, Vec2), circle: (Vec2, f32)) -> bool {
+
+	let (p1, p2) = line;
+
+	if point_circle(p1, circle) || point_circle(p2, circle) {
+		return true;
+	}
+
+	return false;
+
+}
+
+// TODO
+fn point_line(pt: Vec2, line: (Vec2, Vec2)) -> bool {
+
+	let (p1, p2) = line;
+
+	// get distance from the point to the two ends of the line
+	let d1 = Vec2::dis(pt, p1);
+	let d2 = Vec2::dis(pt, p2);
+
+	// get the length of the line
+	let len = Vec2::dis(p1, p2);
+
+	return false;
+
+}
+
+fn point_point(p1: Vec2, p2: Vec2) -> bool {
+	return p1 == p2;
+}
+
+fn circle_circle(c1: (Vec2, f32), c2: (Vec2, f32)) -> bool {
+
+	let (p1, r1) = c1;
+	let (p2, r2) = c2;
+
+	return r1 + r2 < Vec2::dis(p1, p2);
+
 }
 
 fn line_line(l1: (Vec2, Vec2), l2: (Vec2, Vec2)) -> bool {
@@ -82,8 +168,9 @@ fn poly_poly(poly1: &[Vec2], poly2: &[Vec2]) -> bool {
 
 }
 
-fn point_rect(p: Vec2, rect: Quad) -> bool {
-	return p.x >= rect.x && p.x <= rect.x + rect.w && p.y >= rect.y && p.y <= rect.y + rect.h;
+fn point_rect(pt: Vec2, rect: (Vec2, Vec2)) -> bool {
+	let (p1, p2) = rect;
+	return pt.x >= p1.x && pt.x <= p2.x && pt.y >= p1.y && pt.y <= p2.y;
 }
 
 fn point_poly(p: Vec2, poly: &[Vec2]) -> bool {
@@ -203,26 +290,115 @@ pub fn overlaps(s1: Shape2D, s2: Shape2D) -> bool {
 		Circle(center, radius) => {
 			match s2 {
 				Circle(center2, radius2) => {
-					return circle_circle(center, radius, center2, radius2);
-				}
+					return circle_circle((center, radius), (center2, radius2));
+				},
 				Rect(p1, p2) => {
-					return rect_circle(p1, p2, center, radius);
-				}
+					return rect_circle((p1, p2), (center, radius));
+				},
 				Point(pt) => {
-					// ...
-				}
+					return point_circle(pt, (center, radius));
+				},
 				Line(p1, p2) => {
-					// ...
-				}
+					return line_circle((p1, p2), (center, radius));
+				},
 				Polygon(verts) => {
-					// ...
-				}
+					// TODO
+				},
 				Ray(pt, angle) => {
-					// ...
-				}
+					// TODO
+				},
 			}
-		}
-		_ => {},
+		},
+		Rect(p1, p2) => {
+			match s2 {
+				Circle(..) => {
+					return overlaps(s2, s1);
+				},
+				Rect(p12, p22) => {
+					return rect_rect((p1, p2), (p12, p22));
+				},
+				Point(pt) => {
+					return point_rect(pt, (p1, p2));
+				},
+				Line(p12, p22) => {
+					return line_rect((p12, p22), (p1, p2));
+				},
+				Polygon(verts) => {
+					return poly_poly(&verts, &[p1, vec2!(p2.x, p1.y), p2, vec2!(p1.x, p2.y)]);
+				},
+				Ray(pt, angle) => {
+					// TODO
+				},
+			}
+		},
+		Point(pt) => {
+			match s2 {
+				Circle(..)
+				| Rect(..) => {
+					return overlaps(s2, s1);
+				},
+				Point(pt2) => {
+					return point_point(pt, pt2);
+				},
+				Line(p1, p2) => {
+					return point_line(pt, (p1, p2));
+				},
+				Polygon(verts) => {
+					return point_poly(pt, &verts);
+				},
+				Ray(pt, angle) => {
+					// TODO
+				},
+			}
+		},
+		Line(p1, p2) => {
+			match s2 {
+				Circle(..)
+				| Rect(..)
+				| Point(..) => {
+					return overlaps(s2, s1);
+				},
+				Line(p12, p22) => {
+					return line_line((p1, p2), (p12, p22));
+				},
+				Polygon(verts) => {
+					return line_poly((p1, p2), &verts);
+				},
+				Ray(pt, angle) => {
+					// TODO
+				},
+			}
+		},
+		Polygon(ref verts) => {
+			match s2 {
+				Circle(..)
+				| Rect(..)
+				| Point(..)
+				| Line(..) => {
+					return overlaps(s2, s1);
+				},
+				Polygon(verts2) => {
+					return poly_poly(&verts, &verts2);
+				},
+				Ray(pt, angle) => {
+					// TODO
+				},
+			}
+		},
+		Ray(pt, angle) => {
+			match s2 {
+				Circle(..)
+				| Rect(..)
+				| Point(..)
+				| Line(..)
+				| Polygon(..) => {
+					return overlaps(s2, s1);
+				},
+				Ray(pt, angle) => {
+					// TODO
+				},
+			}
+		},
 	}
 
 	return false;
