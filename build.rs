f2029f2541b5e9786e09a32111ab2a1722fff199
tga <tga@space55.xyz>
2019-08-24 05:42:46 -0400

    fix build script

--- build.rs
+++ build.rs
@@ -18,95 +18,76 @@ enum Path {
 	All(&'static str),
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-enum Action {
-	Link(Lib),
-	Search(Path),
-	Flags(&'static str),
-	Cfg(&'static str),
-	Env(&'static str, &'static str),
-}
+fn link(lib: Lib) {
 
-fn perform(ops: Action) {
+	use Lib::*;
 
-	use Action::*;
+	let kind;
+	let name;
 
-	match ops {
-
-		Link(lib) => {
-
-			use Lib::*;
-
-			let kind;
-			let name;
-
-			match lib {
-				Static(p) => {
-					kind = "static";
-					name = p;
-				},
-				Dylib(p) => {
-					kind = "Dylib";
-					name = p;
-				},
-				Framework(p) => {
-					kind = "framework";
-					name = p;
-				},
-			}
-
-			println!("cargo:rustc-link={}={}", kind, name);
-
+	match lib {
+		Static(p) => {
+			kind = "static";
+			name = p;
 		},
-
-		Search(search) => {
-
-			use Path::*;
-
-			let kind;
-			let path;
-
-			match search {
-				Dependency(p) => {
-					kind = "dependency";
-					path = p;
-				},
-				Crate(p) => {
-					kind = "crate";
-					path = p;
-				},
-				Native(p) => {
-					kind = "native";
-					path = p;
-				},
-				Framework(p) => {
-					kind = "framework";
-					path = p;
-				},
-				All(p) => {
-					kind = "all";
-					path = p;
-				},
-			}
-
-			println!("cargo:rustc-search={}={}", kind, path);
-
+		Dylib(p) => {
+			kind = "Dylib";
+			name = p;
 		},
-
-		Flags(f) => {
-			println!("cargo:rustc-flags={}", f);
+		Framework(p) => {
+			kind = "framework";
+			name = p;
 		},
-
-		Cfg(c) => {
-			println!("cargo:rustc-cfg={}", c);
-		},
-
-		Env(k, v) => {
-			println!("cargo:rustc-env={}={}", k, v);
-		},
-
 	}
 
+	println!("cargo:rustc-link-lib={}={}", kind, name);
+
+}
+
+fn search(search: Path) {
+
+	use Path::*;
+
+	let kind;
+	let path;
+
+	match search {
+		Dependency(p) => {
+			kind = "dependency";
+			path = p;
+		},
+		Crate(p) => {
+			kind = "crate";
+			path = p;
+		},
+		Native(p) => {
+			kind = "native";
+			path = p;
+		},
+		Framework(p) => {
+			kind = "framework";
+			path = p;
+		},
+		All(p) => {
+			kind = "all";
+			path = p;
+		},
+	}
+
+	println!("cargo:rustc-link-search={}={}", kind, path);
+
+}
+
+fn flags(f: &str) {
+	println!("cargo:rustc-flags={}", f);
+}
+
+fn cfg(c: &str) {
+	println!("cargo:rustc-cfg={}", c);
+}
+
+fn env(k: &str, v: &str) {
+	println!("cargo:rustc-env={}={}", k, v);
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -140,9 +121,71 @@ impl OS {
 	}
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Family {
+	Windows,
+	Unix,
+}
+
+impl Family {
+	fn from_str(s: &str) -> Option<Self> {
+		use Family::*;
+		return match s {
+			"windows" => Some(Windows),
+			"unix" => Some(Unix),
+			_ => None,
+		};
+	}
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Arch {
+	X86,
+	X86_64,
+	Mips,
+	PowerPC,
+	PowerPC64,
+	Arm,
+	AArch64,
+	Wasm32,
+}
+
+impl Arch {
+	fn from_str(s: &str) -> Option<Self> {
+		use Arch::*;
+		return match s {
+			"x86" => Some(X86),
+			"x86_64" => Some(X86_64),
+			"mips" => Some(Mips),
+			"powerpc" => Some(PowerPC),
+			"powerpc64" => Some(PowerPC64),
+			"arm" => Some(Arm),
+			"aarch64" => Some(AArch64),
+			"wasm32" => Some(Wasm32),
+			_ => None,
+		};
+	}
+}
+
 fn target_os() -> Option<OS> {
-	if let Ok(target_os) = env::var("CARGO_CFG_TARGET_OS") {
-		return OS::from_str(&target_os);
+	if let Ok(os) = env::var("CARGO_CFG_TARGET_OS") {
+		return OS::from_str(&os);
+	} else {
+		return None;
+	}
+}
+
+fn target_family() -> Option<Family> {
+	if let Ok(fam) = env::var("CARGO_CFG_TARGET_FAMILY") {
+		return Family::from_str(&fam);
+	} else {
+		return None;
+	}
+}
+
+fn target_arch() -> Option<Arch> {
+	if let Ok(arch) = env::var("CARGO_CFG_TARGET_ARCH") {
+		return Arch::from_str(&arch);
 	} else {
 		return None;
 	}
@@ -152,7 +195,7 @@ fn main() {
 
 	if let Some(os) = target_os() {
 		if os == OS::IOS {
-			perform(Action::Link(Lib::Framework("OpenGLES")));
+			link(Lib::Framework("OpenGLES"));
 		}
 	}
 
