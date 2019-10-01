2a6413d4c2dd5625286c1ae22f729b497dc0b643
tga <tga@wengwengweng.me>
2019-10-01 18:53:52 -0400

    add imgui support

--- src/app/gfx.rs
+++ src/app/gfx.rs
@@ -369,14 +369,12 @@ impl VertexLayout for Vertex3D {
 	}
 
 	fn attrs() -> gl::VertexAttrGroup {
-
 		return &[
 			("pos", 3),
 			("uv", 2),
 			("normal", 3),
 			("color", 4),
 		];
-
 	}
 
 }
@@ -881,6 +879,88 @@ impl PerspectiveCam {
 impl Camera for PerspectiveCam {
 	fn projection(&self) -> Mat4 {
 		return math::perspective(self.fov.to_radians(), self.aspect, self.near, self.far);
+	}
+	fn lookat(&self) -> Mat4 {
+		return math::lookat(self.pos, self.pos + self.front, vec3!(0, 1, 0));
+	}
+}
+
+#[derive(Clone)]
+pub struct OrthoCam {
+	front: Vec3,
+	pos: Vec3,
+	yaw: f32,
+	pitch: f32,
+	width: f32,
+	height: f32,
+	near: f32,
+	far: f32,
+}
+
+impl OrthoCam {
+
+	pub fn new(width: f32, height: f32, near: f32, far: f32, pos: Vec3, yaw: f32, pitch: f32) -> Self {
+
+		let mut c = Self {
+			pos: vec3!(),
+			front: vec3!(),
+			yaw: 0.0,
+			pitch: 0.0,
+			width: width,
+			height: height,
+			near: near,
+			far: far,
+		};
+
+		c.set_pos(pos);
+		c.set_angle(yaw, pitch);
+
+		return c;
+
+	}
+
+	pub fn set_pos(&mut self, pos: Vec3) {
+		self.pos = pos;
+	}
+
+	pub fn set_front(&mut self, front: Vec3) {
+		self.front = front;
+	}
+
+	pub fn set_angle(&mut self, yaw: f32, pitch: f32) {
+
+		self.yaw = yaw;
+		self.pitch = pitch;
+
+		self.front = vec3!(
+			self.pitch.cos() * (self.yaw + 90f32.to_radians()).cos(),
+			self.pitch.sin(),
+			self.pitch.cos() * (self.yaw + 90f32.to_radians()).sin(),
+		).normalize();
+
+	}
+
+	pub fn front(&self) -> Vec3 {
+		return self.front;
+	}
+
+	pub fn pos(&self) -> Vec3 {
+		return self.pos;
+	}
+
+	pub fn yaw(&self) -> f32 {
+		return self.yaw;
+	}
+
+	pub fn pitch(&self) -> f32 {
+		return self.pitch;
+	}
+
+}
+
+impl Camera for OrthoCam {
+	fn projection(&self) -> Mat4 {
+		return math::ortho(-self.width / 2.0, self.width / 2.0, self.height / 2.0, -self.height / 2.0, self.near, self.far);
 	}
 	fn lookat(&self) -> Mat4 {
 		return math::lookat(self.pos, self.pos + self.front, vec3!(0, 1, 0));
