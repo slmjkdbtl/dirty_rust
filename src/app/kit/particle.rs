6ac506e68fc6e6744b81a3d91d98a65e75ef5aaa
tga <tga@wengwengweng.me>
2019-11-18 23:17:42 -0500

    improve particle system

--- src/app/kit/particle.rs
+++ src/app/kit/particle.rs
@@ -13,7 +13,10 @@ pub struct Particle {
 	acc: Vec2,
 	vel: Vec2,
 	speed: f32,
-	color: (Color, Color),
+	color_start: Color,
+	color_end: Color,
+	size_start: f32,
+	size_end: f32,
 	dead: bool,
 }
 
@@ -37,9 +40,13 @@ impl gfx::Drawable for Particle {
 
 	fn draw(&self, ctx: &mut Ctx) -> Result<()> {
 
+		let t = self.timer.progress();
+		let size = self.size_start.lerp(self.size_end, t);
+		let color = self.color_start.lerp(self.color_end, t);
+
 		ctx.draw(
-			&shapes::rect(self.pos - vec2!(1), self.pos + vec2!(1))
-				.fill(self.color.0)
+			&shapes::rect(self.pos - vec2!(size) / 2.0, self.pos + vec2!(size) / 2.0)
+				.fill(color)
 		)?;
 
 		return Ok(());
@@ -52,11 +59,15 @@ pub struct ParticleConf {
 	pos: Vec2,
 	offset: (Vec2, Vec2),
 	life: (f32, f32),
-	color: (Color, Color),
+	color_start: (Color, Color),
+	color_end: Color,
 	speed: (f32, f32),
 	acc: (Vec2, Vec2),
 	vel: (Vec2, Vec2),
 	rate: (f32, f32),
+	size_start: (f32, f32),
+	size_end: (f32, f32),
+	num: (usize, usize),
 }
 
 impl Default for ParticleConf {
@@ -64,12 +75,16 @@ impl Default for ParticleConf {
 		return Self {
 			pos: vec2!(),
 			offset: (vec2!(0), vec2!(0)),
-			life: (1.6, 2.4),
-			color: (rgba!(1), rgba!(1, 1, 1, 0)),
-			speed: (96.0, 120.0),
+			life: (3.0, 5.0),
+			color_start: (rgba!(0.9, 0.3, 0, 0.4), rgba!(1, 0.3, 0, 0.5)),
+			color_end: rgba!(0.2, 0.2, 1, 0),
+			speed: (96.0, 240.0),
 			acc: (vec2!(0, 2), vec2!(0, 2)),
-			vel: (vec2!(-1, -1), vec2!(1, -1)),
+			vel: (vec2!(-0.5, -1.2), vec2!(0.5, -1.2)),
 			rate: (0.02, 0.05),
+			size_start: (12.0, 36.0),
+			size_end: (0.0, 0.0),
+			num: (12, 16),
 		};
 	}
 }
@@ -91,24 +106,48 @@ impl ParticleSystemBuilder {
 		self.conf.life = (min, max);
 		return self;
 	}
-	pub fn acc(mut self, min: Vec2, max: Vec2) -> Self {
-		self.conf.acc = (min, max);
+	pub fn color_start(mut self, min: Color, max: Color) -> Self {
+		self.conf.color_start = (min, max);
+		return self;
+	}
+	pub fn color_end(mut self, c: Color) -> Self {
+		self.conf.color_end = c;
 		return self;
 	}
 	pub fn speed(mut self, min: f32, max: f32) -> Self {
 		self.conf.speed = (min, max);
 		return self;
 	}
-	pub fn color(mut self, c1: Color, c2: Color,) -> Self {
-		self.conf.color = (c1, c2);
+	pub fn acc(mut self, min: Vec2, max: Vec2) -> Self {
+		self.conf.acc = (min, max);
+		return self;
+	}
+	pub fn vel(mut self, min: Vec2, max: Vec2) -> Self {
+		self.conf.vel = (min, max);
+		return self;
+	}
+	pub fn rate(mut self, min: f32, max: f32) -> Self {
+		self.conf.rate = (min, max);
+		return self;
+	}
+	pub fn size_start(mut self, min: f32, max: f32) -> Self {
+		self.conf.size_start = (min, max);
+		return self;
+	}
+	pub fn size_end(mut self, min: f32, max: f32) -> Self {
+		self.conf.size_end = (min, max);
+		return self;
+	}
+	pub fn num(mut self, min: usize, max: usize) -> Self {
+		self.conf.num = (min, max);
 		return self;
 	}
 	pub fn build(self) -> ParticleSystem {
 		return ParticleSystem {
 			particles: Vec::with_capacity(256),
-			spawn_timer: Timer::new(rand(self.conf.rate)),
+			spawn_timer: Timer::new(rand_t(self.conf.rate)),
 			conf: self.conf,
-			emitting: true,
+			paused: false,
 		};
 	}
 }
@@ -117,7 +156,7 @@ pub struct ParticleSystem {
 	particles: Vec<Particle>,
 	conf: ParticleConf,
 	spawn_timer: Timer,
-	emitting: bool,
+	paused: bool,
 }
 
 impl ParticleSystem {
@@ -130,10 +169,10 @@ impl ParticleSystem {
 
 	pub fn update(&mut self, dt: f32) {
 
-		if self.emitting {
+		if !self.paused {
 			if self.spawn_timer.tick(dt) {
-				self.spawn_timer.reset_to(rand(self.conf.rate));
-				self.spawn();
+				self.spawn_timer.reset_to(rand_t(self.conf.rate));
+				self.emit();
 			}
 		}
 
@@ -149,31 +188,43 @@ impl ParticleSystem {
 		return self.particles.len();
 	}
 
+	pub fn active(&self) -> bool {
+		return !self.paused;
+	}
+
 	pub fn pause(&mut self) {
-		self.emitting = false;
+		self.paused = true;
 	}
 
 	pub fn start(&mut self) {
-		self.emitting = true;
+		self.paused = false;
 	}
 
 	pub fn set_pos(&mut self, p: Vec2) {
 		self.conf.pos = p;
 	}
 
-	fn spawn(&mut self) {
+	pub fn emit(&mut self) {
 
-		let p = Particle {
-			timer: Timer::new(rand(self.conf.life)),
-			pos: self.conf.pos + rand(self.conf.offset),
-			acc: rand(self.conf.acc),
-			vel: rand(self.conf.vel),
-			speed: rand(self.conf.speed),
-			color: self.conf.color,
-			dead: false,
-		};
+		for _ in 0..rand_t(self.conf.num) {
 
-		self.particles.push(p);
+
+			let p = Particle {
+				timer: Timer::new(rand_t(self.conf.life)),
+				pos: self.conf.pos + rand_t(self.conf.offset),
+				acc: rand_t(self.conf.acc),
+				vel: rand_t(self.conf.vel),
+				speed: rand_t(self.conf.speed),
+				color_start: rand_t(self.conf.color_start),
+				color_end: self.conf.color_end,
+				size_start: rand_t(self.conf.size_start),
+				size_end: rand_t(self.conf.size_end),
+				dead: false,
+			};
+
+			self.particles.push(p);
+
+		}
 
 	}
 
