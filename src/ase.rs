1e98c621ebb62e6345c84cb683a4aa14b672c7a1
tga <tga@space55.xyz>
2019-12-25 21:44:22 -0500

    remove ase bin

--- src/ase.rs
+++ src/ase.rs
@@ -1,7 +1,5 @@
 // wengwengweng
 
-use std::path::Path;
-use std::io::Cursor;
 use std::collections::HashMap;
 
 use crate::math::*;
@@ -9,8 +7,8 @@ use crate::Result;
 
 ///! Load Aseprite Spritesheets
 
-#[derive(Copy, Clone, Debug)]
 /// anim
+#[derive(Copy, Clone, Debug)]
 pub struct Anim {
 
 	/// from frame
@@ -21,6 +19,7 @@ pub struct Anim {
 }
 
 /// sprite data
+#[derive(Clone, Debug)]
 pub struct SpriteData {
 
 	/// frames
@@ -30,63 +29,58 @@ pub struct SpriteData {
 
 }
 
-impl SpriteData {
+#[cfg(feature = "json")]
+pub fn parse(json: &str) -> Result<SpriteData> {
 
-	#[cfg(feature = "json")]
-	pub fn from_json(json: &str) -> Result<Self> {
+	use crate::codec::json;
 
-		use crate::codec::json;
+	let mut frames = vec![];
+	let mut anims = HashMap::new();
+	let data: aseprite::SpritesheetData = json::decode(json)?;
 
-		let mut frames = vec![];
-		let mut anims = HashMap::new();
-		let data: ase_json::SpritesheetData = json::decode(json)?;
+	let width = data.meta.size.w;
+	let height = data.meta.size.h;
 
-		let width = data.meta.size.w;
-		let height = data.meta.size.h;
+	for f in data.frames {
 
-		for f in data.frames {
-
-			frames.push(Quad::new(
-				f.frame.x as f32 / width as f32,
-				f.frame.y as f32 / height as f32,
-				f.frame.w as f32 / width as f32,
-				f.frame.h as f32 / height as f32
-			));
-
-		}
-
-		if let Some(frame_tags) = data.meta.frame_tags {
-
-			for anim in frame_tags {
-
-				let mut from = anim.from;
-				let mut to = anim.to;
-
-				if let ase_json::Direction::Reverse = anim.direction {
-					std::mem::swap(&mut from, &mut to);
-				}
-
-				anims.insert(anim.name, Anim {
-					from: from as usize,
-					to: to as usize,
-				});
-
-			}
-
-		}
-
-		return Ok(SpriteData {
-			frames: frames,
-			anims: anims,
-		});
+		frames.push(Quad::new(
+			f.frame.x as f32 / width as f32,
+			f.frame.y as f32 / height as f32,
+			f.frame.w as f32 / width as f32,
+			f.frame.h as f32 / height as f32
+		));
 
 	}
 
+	if let Some(frame_tags) = data.meta.frame_tags {
+
+		for anim in frame_tags {
+
+			let mut from = anim.from;
+			let mut to = anim.to;
+
+			if let aseprite::Direction::Reverse = anim.direction {
+				std::mem::swap(&mut from, &mut to);
+			}
+
+			anims.insert(anim.name, Anim {
+				from: from as usize,
+				to: to as usize,
+			});
+
+		}
+
+	}
+
+	return Ok(SpriteData {
+		frames: frames,
+		anims: anims,
+	});
+
 }
 
-pub use ase_bin::Aseprite;
-
-fn parse(b: &[u8]) -> Result<Aseprite> {
-	return Ok(ase_bin::Aseprite::from_read(&mut Cursor::new(b))?);
+#[cfg(not(feature = "json"))]
+pub fn parse(json: &str) -> Result<SpriteData> {
+	return compile_error!("requires the 'json' feature");
 }
 
