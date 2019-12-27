846b3ef747d3d5867e15a2c575bd7d9549e5618e
tga <tga@space55.xyz>
2019-12-27 15:16:53 -0500

    canvas give original sizes

--- src/app/canvas.rs
+++ src/app/canvas.rs
@@ -7,37 +7,10 @@ use crate::*;
 use super::*;
 use super::gfx::*;
 
-#[derive(Clone)]
-pub struct CanvasBuilder {
-	origin: Option<Origin>,
-	size: Option<(i32, i32)>,
-}
-
-impl CanvasBuilder {
-	pub fn origin(mut self, o: Origin) -> Self {
-		self.origin = Some(o);
-		return self;
-	}
-	pub fn size(mut self, w: i32, h: i32) -> Self {
-		self.size = Some((w, h));
-		return self;
-	}
-	pub fn build(self, ctx: &Ctx) -> Result<Canvas> {
-
-		let (w, h) = self.size.unwrap_or((ctx.gwidth() as i32, ctx.gheight() as i32));
-		let dpi = ctx.dpi();
-		let tw = (w as f32 * dpi) as i32;
-		let th = (h as f32 * dpi) as i32;
-		let fbuf = gl::Framebuffer::new(&ctx.gl, tw, th)?;
-		let tex = Texture::from_gl_tex(fbuf.tex().clone());
-
-		return Ok(Canvas {
-			gl_fbuf: Rc::new(fbuf),
-			tex: tex,
-			origin: self.origin.unwrap_or(ctx.conf.origin),
-		});
-
-	}
+#[derive(Clone, Copy, Debug)]
+pub struct CanvasConf {
+	size: (i32, i32),
+	origin: Origin,
 }
 
 /// framebuffer for off-screen rendering
@@ -46,35 +19,48 @@ pub struct Canvas {
 	gl_fbuf: Rc<gl::Framebuffer>,
 	tex: Texture,
 	origin: Origin,
+	size: (i32, i32),
 }
 
 impl Canvas {
 
-	pub fn builder() -> CanvasBuilder {
-		return CanvasBuilder {
-			origin: None,
-			size: None,
-		};
+	pub fn from_conf(ctx: &Ctx, conf: &CanvasConf) -> Result<Self> {
+
+		let dpi = ctx.dpi();
+		let (w, h) = conf.size;
+		let tw = (w as f32 * dpi) as i32;
+		let th = (h as f32 * dpi) as i32;
+		let fbuf = gl::Framebuffer::new(&ctx.gl, tw, th)?;
+		let tex = Texture::from_gl_tex(fbuf.tex().clone());
+
+		return Ok(Self {
+			gl_fbuf: Rc::new(fbuf),
+			tex: tex,
+			origin: conf.origin,
+			size: conf.size,
+		});
+
 	}
 
 	/// create a new canvas from width and height
 	pub fn new(ctx: &Ctx, width: i32, height: i32) -> Result<Self> {
 
-		return Self::builder()
-			.size(width, height)
-			.build(ctx);
+		return Self::from_conf(ctx, &CanvasConf {
+			size: (width, height),
+			origin: ctx.conf.origin,
+		});
 
 	}
 
 	// TODO: give original size
 	/// get canvas width
 	pub fn width(&self) -> i32 {
-		return self.tex.width();
+		return self.size.0;
 	}
 
 	/// get canvas height
 	pub fn height(&self) -> i32 {
-		return self.tex.height();
+		return self.size.1;
 	}
 
 	/// get underlying texture for the canvas
