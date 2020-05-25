7a5f4673a09b0229fb368cadc9b0b8edd1126124
tga <tga@space55.xyz>
2020-05-25 05:34:23 -0400

    clean

--- src/audio/buffer.rs
+++ src/audio/buffer.rs
@@ -9,34 +9,53 @@ use super::*;
 pub(super) struct Buffered {
 	buf: Arc<Vec<Frame>>,
 	sample_rate: u32,
-	cur_idx: usize,
+	cur_pos: usize,
+	duration: Duration,
 }
 
 impl Buffered {
+
 	pub fn new(src: impl Source) -> Self {
+
+		let sample_rate = src.sample_rate();
+		let buf = src.into_iter().collect::<Vec<Frame>>();
+
 		return Self {
-			sample_rate: src.sample_rate(),
-			buf: Arc::new(src.into_iter().collect()),
-			cur_idx: 0,
+			sample_rate: sample_rate,
+			duration: Duration::from_secs_f32(buf.len() as f32 / sample_rate as f32),
+			buf: Arc::new(buf),
+			cur_pos: 0,
 		};
+
 	}
+
 	pub fn duration(&self) -> Duration {
-		return Duration::from_secs_f32(self.buf.len() as f32 / self.sample_rate as f32);
+		return self.duration;
 	}
+
+	pub fn reset(&mut self) {
+		self.cur_pos = 0;
+	}
+
 }
 
 impl Iterator for Buffered {
+
 	type Item = Frame;
+
 	fn next(&mut self) -> Option<Self::Item> {
-		let v = self.buf.get(self.cur_idx).map(|f| *f);
-		self.cur_idx += 1;
+		let v = self.buf.get(self.cur_pos).map(|f| *f);
+		self.cur_pos += 1;
 		return v;
 	}
+
 }
 
 impl Source for Buffered {
+
 	fn sample_rate(&self) -> u32 {
 		return self.sample_rate;
 	}
+
 }
 
