a132a9a1ee0044a87d39cad4fad9c596770c03d1
tga <tga@wengwengweng.me>
2020-05-25 12:20:04 -0400

    add Image::resize and make Canvas::capture return an Image

---
+++ examples/canvas.rs
@@ -0,0 +1,73 @@
+// wengwengweng
+
+use dirty::*;
+use gfx::shapes;
+use input::Key;
+
+struct Game {
+	canvas1: gfx::Canvas,
+}
+
+impl State for Game {
+
+	fn init(d: &mut Ctx) -> Result<Self> {
+		return Ok(Self {
+			canvas1: gfx::Canvas::new(d.gfx, 100, 100)?,
+		});
+	}
+
+	fn event(&mut self, d: &mut Ctx, e: &input::Event) -> Result<()> {
+
+		use input::Event::*;
+
+		match e {
+			KeyPress(k) => {
+				match *k {
+					Key::Esc => d.window.quit(),
+					_ => {},
+				}
+			},
+			_ => {},
+		}
+
+		return Ok(());
+
+	}
+
+	fn draw(&mut self, d: &mut Ctx) -> Result<()> {
+
+		use gfx::Vertex;
+
+		d.gfx.draw(&shapes::raw(&[
+			Vertex {
+				pos: vec3!(0, 72, 0),
+				color: rgba!(1, 0, 0, 1),
+				normal: vec3!(0, 0, 1),
+				uv: vec2!(0),
+			},
+			Vertex {
+				pos: vec3!(-96, -72, 0),
+				color: rgba!(0, 1, 0, 1),
+				normal: vec3!(0, 0, 1),
+				uv: vec2!(0),
+			},
+			Vertex {
+				pos: vec3!(96, -72, 0),
+				color: rgba!(0, 0, 1, 1),
+				normal: vec3!(0, 0, 1),
+				uv: vec2!(0),
+			},
+		], &[0, 1, 2]))?;
+
+		return Ok(());
+
+	}
+
+}
+
+fn main() {
+	if let Err(e) = run::<Game>() {
+		elog!("{}", e);
+	}
+}
+
