92b01616b1015599ba0032f6bd043d3edf5c2c9d
tga <tga@space55.xyz>
2020-06-09 21:59:22 -0400

    revert false changes to fs::bundled_path, remove unnecessary methods

--- src/fs.rs
+++ src/fs.rs
@@ -16,7 +16,9 @@ use crate::Result;
 #[cfg(target_os = "macos")]
 pub fn res_dir() -> Option<PathBuf> {
 
-	let bundle = core_foundation::bundle::CFBundle::main_bundle();
+	use core_foundation::bundle;
+
+	let bundle = bundle::CFBundle::main_bundle();
 
 	let path = bundle
 		.executable_url()?
@@ -44,22 +46,34 @@ pub fn res_dir() -> Option<PathBuf> {
 
 }
 
-/// get the path that's prepended with the res dir
-pub fn bundled_path(path: impl AsRef<Path>) -> PathBuf {
+/// get the path that's prepended with the res dir, if it doesn't exit then return the input path back
+pub fn bundled_path(path: impl AsRef<Path>) -> Result<PathBuf> {
 
 	let path = path.as_ref();
 
-	if let Some(res_dir) = res_dir() {
-		return res_dir.join(path);
+	if path.exists() {
+
+		return Ok(path.to_path_buf());
+
 	} else {
-		return path.to_path_buf();
+
+		let with_res = res_dir()
+			.ok_or(format!("file not found: {}", path.display()))?
+			.join(path);
+
+		if with_res.exists() {
+			return Ok(with_res);
+		} else {
+			return Err(format!("file not found: {}", path.display()));
+		}
+
 	}
 
 }
 
 /// check if a file exists
 pub fn exists(path: impl AsRef<Path>) -> bool {
-	return bundled_path(path).exists();
+	return bundled_path(path).is_ok();
 }
 
 /// get files that matches a glob pattern (e.g. `glob("img/*.png")`)
@@ -80,7 +94,7 @@ pub fn glob(pat: &str) -> Result<Vec<PathBuf>> {
 pub fn read(path: impl AsRef<Path>) -> Result<Vec<u8>> {
 
 	let path = path.as_ref();
-	let path = bundled_path(path);
+	let path = bundled_path(path)?;
 
 	return fs::read(&path)
 		.map_err(|_| format!("failed to read file {}", path.display()));
@@ -91,21 +105,10 @@ pub fn read(path: impl AsRef<Path>) -> Result<Vec<u8>> {
 pub fn read_str(path: impl AsRef<Path>) -> Result<String> {
 
 	let path = path.as_ref();
-	let path = bundled_path(path);
+	let path = bundled_path(path)?;
 
 	return fs::read_to_string(&path)
 		.map_err(|_| format!("failed to read file {}", path.display()));
-
-}
-
-/// get directory listing
-pub fn read_dir(path: impl AsRef<Path>) -> Result<std::fs::ReadDir> {
-
-	let path = path.as_ref();
-	let path = bundled_path(path);
-
-	return fs::read_dir(&path)
-		.map_err(|_| format!("failed to read dir {}", path.display()));
 
 }
 
@@ -113,7 +116,7 @@ pub fn read_dir(path: impl AsRef<Path>) -> Result<std::fs::ReadDir> {
 pub fn basename(path: impl AsRef<Path>) -> Result<String> {
 
 	let path = path.as_ref();
-	let path = bundled_path(path);
+	let path = bundled_path(path)?;
 
 	return Ok(
 		Path::new(&path)
@@ -130,6 +133,7 @@ pub fn basename(path: impl AsRef<Path>) -> Result<String> {
 pub fn extname(path: impl AsRef<Path>) -> Result<String> {
 
 	let path = path.as_ref();
+	let path = bundled_path(path)?;
 
 	return Ok(path
 		.extension()
@@ -138,89 +142,6 @@ pub fn extname(path: impl AsRef<Path>) -> Result<String> {
 		.into_string()
 		.map_err(|_| format!("failed to get extname: {}", path.display()))?
 	);
-
-}
-
-/// copy file
-pub fn copy(p1: impl AsRef<Path>, p2: impl AsRef<Path>) -> Result<u64> {
-
-	let p1 = p1.as_ref();
-	let p2 = p2.as_ref();
-
-	return fs::copy(p1, p2)
-		.map_err(|_| format!("failed to copy {} to {}", p1.display(), p2.display()));
-
-}
-
-/// create directory
-pub fn mkdir(path: impl AsRef<Path>) -> Result<()> {
-
-	let path = path.as_ref();
-
-	return fs::create_dir_all(path)
-		.map_err(|_| format!("failed to create directory {}", path.display()));
-
-}
-
-/// check if a path is a file
-pub fn is_file(path: impl AsRef<Path>) -> bool {
-	return path.as_ref().is_file();
-}
-
-/// check if a path is a directory
-pub fn is_dir(path: impl AsRef<Path>) -> bool {
-	return path.as_ref().is_dir();
-}
-
-/// remove file
-pub fn remove(path: impl AsRef<Path>) -> Result<()> {
-
-	let path = path.as_ref();
-
-	return fs::remove_file(path)
-		.map_err(|_| format!("failed to remove file {}", path.display()));
-
-}
-
-/// remove directory
-pub fn remove_dir(path: impl AsRef<Path>) -> Result<()> {
-
-	let path = path.as_ref();
-
-	return fs::remove_dir(path)
-		.map_err(|_| format!("failed to remove directory {}", path.display()));
-
-}
-
-/// rename file
-pub fn rename(path: impl AsRef<Path>, new: impl AsRef<Path>) -> Result<()> {
-
-	let path = path.as_ref();
-
-	return fs::rename(path, new)
-		.map_err(|_| format!("failed to rename {}", path.display()));
-
-}
-
-/// write to a file
-pub fn write(path: impl AsRef<Path>, content: impl AsRef<[u8]>) -> Result<()> {
-
-	let path = path.as_ref();
-
-	return fs::write(path, content)
-		.map_err(|_| format!("failed to write file {}", path.display()));
-
-}
-
-/// get file size
-pub fn size(path: impl AsRef<Path>) -> Result<u64> {
-
-	let path = path.as_ref();
-	let len = fs::metadata(path)
-		.map_err(|_| format!("failed to read file {}", path.display()))?
-		.len();
-
-	return Ok(len);
 
 }
 
