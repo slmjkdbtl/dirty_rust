9ef99ad6495568e890d048f8f28b6150d15f97c8
tga <tga@space55.xyz>
2020-06-27 17:21:44 -0400

    add political correctness

--- examples/ui.rs
+++ examples/ui.rs
@@ -4,6 +4,65 @@ use dirty::*;
 use gfx::*;
 use input::*;
 
+const GENDERS: [&str; 56] = [
+	"Agender",
+	"Androgyne",
+	"Androgynous",
+	"Bigender",
+	"Cis",
+	"Cisgender",
+	"Cis Female",
+	"Cis Male",
+	"Cis Man",
+	"Cis Woman",
+	"Cisgender Female",
+	"Cisgender Male",
+	"Cisgender Man",
+	"Cisgender Woman",
+	"Female to Male",
+	"FTM",
+	"Gender Fluid",
+	"Gender Nonconforming",
+	"Gender Questioning",
+	"Gender Variant",
+	"Genderqueer",
+	"Intersex",
+	"Male to Female",
+	"MTF",
+	"Neither",
+	"Neutrois",
+	"Non-binary",
+	"Other",
+	"Pangender",
+	"Trans",
+	"Trans*",
+	"Trans Female",
+	"Trans* Female",
+	"Trans Male",
+	"Trans* Male",
+	"Trans Man",
+	"Trans* Man",
+	"Trans Person",
+	"Trans* Person",
+	"Trans Woman",
+	"Trans* Woman",
+	"Transfeminine",
+	"Transgender",
+	"Transgender Female",
+	"Transgender Male",
+	"Transgender Man",
+	"Transgender Person",
+	"Transgender Woman",
+	"Transmasculine",
+	"Transsexual",
+	"Transsexual Female",
+	"Transsexual Male",
+	"Transsexual Man",
+	"Transsexual Person",
+	"Transsexual Woman",
+	"Two-Spirit",
+];
+
 struct Game {
 	ui: ui::UI,
 }
@@ -47,7 +106,7 @@ impl State for Game {
 				p.text("yo")?;
 				p.input("name")?;
 				p.slider::<i32>("age", 18, 0, 100)?;
-				p.select("gender", &["unknown", "male", "female"], 1)?;
+				p.select("gender", &GENDERS, 0)?;
 				p.checkbox("dead", false)?;
 				p.color("color", rgba!(0, 0, 0, 1))?;
 				p.sep()?;
